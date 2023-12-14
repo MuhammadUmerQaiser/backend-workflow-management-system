c55e4133c2c864e9cfc447d19874f429b15740cf
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2023-12-14 14:32:45 +0500

    initial project structure, connection to mongo and dependencies

---
+++ src/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MongoURI = process.env.MONGODB_URI;
+
+const connectToMongo = () => {
+  mongoose.connect(MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+
+  db.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+  db.once("open", () => {
+    console.log("Connected to MongoDB");
+  });
+};
+
+module.exports = connectToMongo;
