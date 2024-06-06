d12eb1f469560859fdec07d6fa929e80c843892b
Muhammad Umer <umerqaiser1009@gmail.com>
2024-06-06 11:45:32 +0500

    'socket'

--- src/index.js
+++ src/index.js
@@ -1,40 +1,38 @@
-const authRoutes = require("./routes/auth")
-const userRoutes = require("./routes/user")
-const adminRoutes = require("./routes/admin")
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectToMongo=require("./db");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectToMongo = require("./db");
+const http = require("http");
+const socketIo = require("socket.io");
 
 connectToMongo();
 const app = express();
 const port = 5000;
 
-const allowedOrigins = ['http://localhost:3000'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
+const allowedOrigins = ["http://localhost:3000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(bodyParser.json());
-app.use("/api",authRoutes)
-app.use("/api",adminRoutes)
-app.use("/api",userRoutes)
-
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", userRoutes);
 
 app.listen(port, () => {
   console.log(`Workflow backend listening on port ${port}`);
 });
-
-
-
-
-
